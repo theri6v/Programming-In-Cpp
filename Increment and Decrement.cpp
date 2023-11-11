89129b5ec74691fdbb881bc166b136f3b6e6b67d
Rishav Walde <rbsrishav25@gmail.com>
2023-11-12 00:20:21 +0530

    Create Increment and Decrement.cpp

---
+++ Increment and Decrement.cpp
@@ -0,0 +1,17 @@
+//progtam for example of increment and decrement operator
+#include<iostream>
+using namespace std;    
+int main()
+{
+    int a = 10, b = 15;
+//increament Operator
+    cout<<"a++ is"<<" "<<a++<<endl;//postfix
+    cout<<"++a is"<<" "<<++a<<endl;//prefix
+
+//decrement Operator
+    cout<<"b--is"<<" "<<b--<<endl;//Postfix
+    cout<<"--b is"<<" "<<--b<<endl;//Prefix
+    
+    return 0;
+}
+    

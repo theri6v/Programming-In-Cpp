f4da643ce257852f29df1f22d747c9c22c8a1419
Rishav Walde <rbsrishav25@gmail.com>
2023-11-12 01:02:16 +0530

    Create Binary Operator.cpp

---
+++ Binary Operator.cpp
@@ -0,0 +1,26 @@
+//Demostate the Binary Operator
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 8, b = 3; 
+    // Aditional Operator
+    cout<<"a + b ="<<" "<<(a+b)<<endl;
+
+    // Subtraction Operator
+    cout<<"a - b ="<<" "<<(a-b)<<endl;
+
+    // Multiplication Operator
+    cout<<"a * b = "<<" "<<(a*b)<<endl;
+
+    // Division Operator
+    cout<<"a / b ="<<" "<<(a/b)<<endl;
+
+    // Modulus Operator
+    cout<<"a % b ="<<" "<<(a%b)<<endl;
+
+    return 0; 
+
+}
